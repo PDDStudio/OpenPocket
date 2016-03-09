6e23d7db49f7df683942bdf2dd73849c22806de0
Patrick J <patrick.pddstudio@googlemail.com>
2016-03-09 13:35:40 +0100

    Removed unused imports

--- app/src/main/java/com/pddstudio/openpocket/MainActivity.java
+++ app/src/main/java/com/pddstudio/openpocket/MainActivity.java
@@ -1,8 +1,5 @@
 package com.pddstudio.openpocket;
 
-import android.graphics.Color;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
