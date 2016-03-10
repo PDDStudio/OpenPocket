5cc1910c53393a83fb29831f0ba6ed6ca9086713
Patrick J <patrick.pddstudio@googlemail.com>
2016-03-10 10:46:37 +0100

    Added gfx from MaterialUp

---
+++ app/src/main/java/com/pddstudio/openpocket/adapters/items/TransactionItem.java
@@ -0,0 +1,9 @@
+package com.pddstudio.openpocket.adapters.items;
+/*
+ * This Class was created by Patrick J
+ * on 10.03.16. For more Details and licensing information
+ * have a look at the README.md
+ */
+
+public class TransactionItem {
+}
