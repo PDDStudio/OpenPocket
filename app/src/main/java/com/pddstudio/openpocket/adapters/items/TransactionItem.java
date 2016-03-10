24f2c07b7f0bcc5202eca4724b8901b26760238c
Patrick J <patrick.pddstudio@googlemail.com>
2016-03-10 10:46:58 +0100

    Begin with TransactionItem implementation

--- app/src/main/java/com/pddstudio/openpocket/adapters/items/TransactionItem.java
+++ app/src/main/java/com/pddstudio/openpocket/adapters/items/TransactionItem.java
@@ -5,5 +5,37 @@ package com.pddstudio.openpocket.adapters.items;
  * have a look at the README.md
  */
 
-public class TransactionItem {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.mikepenz.fastadapter.items.AbstractItem;
+import com.pddstudio.openpocket.R;
+
+public class TransactionItem extends AbstractItem<TransactionItem, TransactionItem.ViewHolder> {
+
+    public TransactionItem() {}
+
+    @Override
+    public int getType() {
+        return R.id.transaction_item;
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.item_transaction;
+    }
+
+    @Override
+    public void bindView(ViewHolder viewHolder) {
+        super.bindView(viewHolder);
+    }
+
+    protected static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+    }
+
 }
