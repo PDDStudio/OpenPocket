83650a08c0ebe535a45caed0c071765a257ee960
Patrick J <patrick.pddstudio@googlemail.com>
2016-03-18 10:25:57 +0100

    Updated TransactionActivity

--- app/src/main/java/com/pddstudio/openpocket/adapters/items/CategoryItem.java
+++ app/src/main/java/com/pddstudio/openpocket/adapters/items/CategoryItem.java
@@ -1,14 +1,13 @@
 package com.pddstudio.openpocket.adapters.items;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.mikepenz.fastadapter.items.AbstractItem;
 import com.mikepenz.iconics.IconicsDrawable;
-import com.mikepenz.materialdrawer.holder.ImageHolder;
 import com.pddstudio.openpocket.R;
-import com.pddstudio.openpocket.views.MLRoundedImageView;
 import com.pddstudio.pocketlibrary.models.Category;
 
 /**
@@ -38,19 +37,16 @@ public class CategoryItem extends AbstractItem<CategoryItem, CategoryItem.ViewHo
     public void bindView(ViewHolder viewHolder) {
         super.bindView(viewHolder);
         // configure the item
-        viewHolder.backgroundImage.setBackgroundColor(viewHolder.backgroundImage.getContext().getResources().getColor(R.color.colorPrimary));
-        IconicsDrawable iconicsDrawable = new IconicsDrawable(viewHolder.categoryImage.getContext()).icon(category.getCategoryIcon());
+        IconicsDrawable iconicsDrawable = new IconicsDrawable(viewHolder.categoryImage.getContext()).icon(category.getCategoryIcon()).color(Color.WHITE);
         viewHolder.categoryImage.setImageDrawable(iconicsDrawable);
     }
 
     protected static class ViewHolder extends RecyclerView.ViewHolder {
 
-        protected MLRoundedImageView backgroundImage;
         protected ImageView categoryImage;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            backgroundImage = (MLRoundedImageView) itemView.findViewById(R.id.categoryBgImage);
             categoryImage = (ImageView) itemView.findViewById(R.id.categoryImage);
         }
     }
