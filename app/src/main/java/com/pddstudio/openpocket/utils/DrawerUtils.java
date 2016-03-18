73f1ccdd9e54ee054a7add9de412409eb21d79fe
Patrick J <patrick.pddstudio@googlemail.com>
2016-03-18 10:02:37 +0100

    Improved Drawer

--- app/src/main/java/com/pddstudio/openpocket/utils/DrawerUtils.java
+++ app/src/main/java/com/pddstudio/openpocket/utils/DrawerUtils.java
@@ -22,18 +22,22 @@ public final class DrawerUtils {
 
     public static List<IDrawerItem> getDrawerItems() {
         List<IDrawerItem> drawerItems = new LinkedList<>();
+        SectionDrawerItem generalSection = new SectionDrawerItem().withDivider(false).withName(R.string.drawer_category_gen);
+        drawerItems.add(generalSection);
         PrimaryDrawerItem homeItem = new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(CommunityMaterial.Icon.cmd_home);
         drawerItems.add(homeItem);
         SectionDrawerItem categorySection = new SectionDrawerItem().withName(R.string.drawer_category_cat);
         drawerItems.add(categorySection);
         for(Category category : OpenPocket.get().getCategoryManager().getCategoryList()) {
-            PrimaryDrawerItem catItem = new PrimaryDrawerItem().withName(category.getCategoryName()).withIcon(IconParser.getIcon(category.getCategoryIcon()));
+            PrimaryDrawerItem catItem = new PrimaryDrawerItem().withName(category.getCategoryName()).withDescription(category.getCategoryDescription()).withIcon(IconParser.getIcon(category.getCategoryIcon()));
             drawerItems.add(catItem);
         }
         DividerDrawerItem dividerItem = new DividerDrawerItem();
+        SectionDrawerItem prefSection = new SectionDrawerItem().withDivider(false).withName(R.string.drawer_category_pref);
         PrimaryDrawerItem aboutItem = new PrimaryDrawerItem().withName(R.string.drawer_item_about).withIcon(CommunityMaterial.Icon.cmd_information_outline);
         PrimaryDrawerItem prefItem = new PrimaryDrawerItem().withName(R.string.drawer_item_pref).withIcon(CommunityMaterial.Icon.cmd_settings);
         drawerItems.add(dividerItem);
+        drawerItems.add(prefSection);
         drawerItems.add(aboutItem);
         drawerItems.add(prefItem);
         return drawerItems;
